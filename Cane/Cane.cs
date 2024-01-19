e7f39a93b9ed48ec8b87653a837d0cde0587c1ff
Tommaso Castagnoli <tommasocastagnoli29@gmail.com>
2024-01-19 19:17:38 +0100

    Aggiungere i file di progetto.

---
+++ Cane/Cane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cane
+{
+    internal class Cane
+    {
+        public string Razza { get; set; }
+        public string Nome { get; set; }
+         
+        public Persona Proprietario{ get; set; }    
+    }
+}
---
+++ Cane/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cane c= new Cane();
+
+            c.Razza = "Labrador";
+            
+            c.Nome = "Ringhio";
+
+ c.Proprietario = new Persona();
+            c.Proprietario.Nome = "Simone";
+
+         
+
+
+            Console.ReadLine();
+        }
+    }
+}
